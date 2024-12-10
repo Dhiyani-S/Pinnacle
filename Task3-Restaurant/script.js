dea6268ca045766a5aee91fbba84da76d2927353
Dhiyani.S <dhiyanisrinivasan123@gmail.com>
2024-12-10 23:46:49 +0530

    Add files via upload

---
+++ Task3-Restaurant/script.js
@@ -0,0 +1,15 @@
+
+document.getElementById("reservation-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+
+    if (!name || !email || !phone || !date || !time) {
+        alert("Please fill out all fields.");
+    } else {
+       alert(`Reservation for ${name} confirmed on ${date} at ${time}. We will contact you at ${phone}.`);
+    }
+});
