006443ab833a4600aa7d10cf379baef512e559fc
Dhiyani.S <dhiyanisrinivasan123@gmail.com>
2024-12-11 14:26:38 +0530

    Update script.js

--- Task3-Restaurant/script.js
+++ Task3-Restaurant/script.js
@@ -1,4 +1,3 @@
-
 document.getElementById("reservation-form").addEventListener("submit", function(event) {
     event.preventDefault(); 
     const name = document.getElementById("name").value;
@@ -6,7 +5,6 @@ document.getElementById("reservation-form").addEventListener("submit", function(
     const phone = document.getElementById("phone").value;
     const date = document.getElementById("date").value;
     const time = document.getElementById("time").value;
-
     if (!name || !email || !phone || !date || !time) {
         alert("Please fill out all fields.");
     } else {
